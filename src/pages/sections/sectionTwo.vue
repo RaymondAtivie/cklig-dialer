17770008a0f5e8752855112cfcf2aa960bd3e554
GIDEON PETERS <gideonpeters85@gmail.com>
2019-09-13 17:53:30 +0100

    made some changes

--- src/pages/sections/sectionTwo.vue
+++ src/pages/sections/sectionTwo.vue
@@ -1,5 +1,61 @@
 <template>
   <div>
-    <h1>Yoooo</h1>
+    <div class="row">
+      <div class="col-4">
+        <div class>
+          <div class="d-flex" v-for="item in items" :key="item.id">
+            <ck-box
+              @click.native="activateBox(item.id)"
+              :class="item.active ? 'border-primary-bg-right': 'border-bg-right'"
+              primary
+              :disabled="!item.active"
+              block
+              mb
+            >{{ item.title }}</ck-box>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
+
+<script>
+export default {
+  data: () => ({
+    items: [
+      {
+        id: 1,
+        title: "שיחות יוצאות",
+        active: false
+      },
+      {
+        id: 2,
+        title: "התראות ועדכונים",
+        active: true
+      },
+      {
+        id: 3,
+        title: " רשימה לבנה - שחורה",
+        active: false
+      },
+      {
+        id: 4,
+        title: "שיחות נכנסות",
+        active: false
+      },
+      {
+        id: 5,
+        title: "נהל הגדרותע",
+        active: false
+      }
+    ]
+  }),
+  methods: {
+    activateBox(id) {
+      this.items.forEach(item => {
+        return item.id === id ? (item.active = true) : (item.active = false);
+      });
+    }
+  }
+};
+</script>
--- src/views/PageTwo.vue
+++ src/views/PageTwo.vue
@@ -1,3 +1,18 @@
 <template>
+  <div>
     <h1>Page Two</h1>
+
+    <div>
+      <section-two />
+    </div>
+  </div>
 </template>
+
+<script>
+import SectionTwo from "./../pages/sections/sectionTwo";
+export default {
+  components: {
+    SectionTwo
+  }
+};
+</script>
