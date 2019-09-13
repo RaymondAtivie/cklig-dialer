62023a5091952765bc0c14fb1d166b6ba88f4d69
GIDEON PETERS <gideonpeters85@gmail.com>
2019-09-13 18:35:09 +0100

    made some changes

--- src/pages/sections/sectionTwo.vue
+++ src/pages/sections/sectionTwo.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <div class="row">
+    <div class="row mb-5">
       <div class="col-4">
         <div class="border-light-bg-left pb-5">
           <div class="pl-2 d-flex" v-for="item in items" :key="item.id">
@@ -13,6 +13,21 @@
               mb
               br
             >{{ item.title }}</ck-box>
+          </div>
+        </div>
+        <div class="d-flex align-content-start bg-light mt-5">
+          <div class="info-icon p-2 ml-2">
+            <icon name="lightbulb" size="1" />
+          </div>
+          <div class="p-3">
+            כל מענה של נתב שיחות / מענה קולי / תא קולי נחשב כמענה לשיחה
+            כלומר, הרשומה טופלה.
+            במקרה כזה השיחה קיבלה מענה טכנולוגי, וזאת למרות שלא ענה
+            נציג אנושי בפועל - ולכן זה נחשב כמענה לשיחה.
+            אם יש לכם נתב שיחות / מענה קולי / תא קולי, מומלץ שהנציג יבצע
+            פעולה כדי לקבל את השיחה - הנציג חייב להקיש 1.
+            חשוב – כל עוד נציג לא הקיש 1 ,זמן ההמתנה ממשיך להיספר
+            ו
           </div>
         </div>
       </div>
@@ -60,3 +75,11 @@ export default {
   }
 };
 </script>
+
+<style lang="scss" scoped>
+.info-icon {
+  background-color: $primary;
+  height: 100%;
+  color: white;
+}
+</style>
