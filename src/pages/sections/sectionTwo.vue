18164c51a5a68d1ca6455525a36cad3668c35acd
GIDEON PETERS <gideonpeters85@gmail.com>
2019-09-13 19:03:02 +0100

    made some changes

--- src/pages/sections/sectionTwo.vue
+++ src/pages/sections/sectionTwo.vue
@@ -17,29 +17,27 @@
         </div>
       </div>
 
-	  <div class="col-6">
+      <div class="col-6">
+        <h2>Switch Me {{active.id}}</h2>
 
-		  <h2>Switch Me {{active.id}}</h2>
-		  
-		  <div v-if="active.id == 4">
-			<div class="d-flex align-content-start bg-light mt-5">
-				<div class="info-icon p-2 ml-2">
-					<icon name="lightbulb" size="1.5" />
-				</div>
-				<p class="p-3 stretch t-secondary">
-					כל מענה של נתב שיחות / מענה קולי / תא קולי נחשב כמענה לשיחה
-					כלומר, הרשומה טופלה.
-					במקרה כזה השיחה קיבלה מענה טכנולוגי, וזאת למרות שלא ענה
-					נציג אנושי בפועל - ולכן זה נחשב כמענה לשיחה.
-					אם יש לכם נתב שיחות / מענה קולי / תא קולי, מומלץ שהנציג יבצע
-					פעולה כדי לקבל את השיחה - הנציג חייב להקיש 1.
-					חשוב – כל עוד נציג לא הקיש 1 ,זמן ההמתנה ממשיך להיספר
-					ו
-				</p>
-			</div>
-		  </div>
-
-	  </div>
+        <div v-if="active.id == 4">
+          <div class="d-flex align-content-start bg-light mt-5">
+            <div class="info-icon p-2 ml-2">
+              <icon name="lightbulb" size="1.5" />
+            </div>
+            <p class="p-3 stretch t-secondary">
+              כל מענה של נתב שיחות / מענה קולי / תא קולי נחשב כמענה לשיחה
+              כלומר, הרשומה טופלה.
+              במקרה כזה השיחה קיבלה מענה טכנולוגי, וזאת למרות שלא ענה
+              נציג אנושי בפועל - ולכן זה נחשב כמענה לשיחה.
+              אם יש לכם נתב שיחות / מענה קולי / תא קולי, מומלץ שהנציג יבצע
+              פעולה כדי לקבל את השיחה - הנציג חייב להקיש 1.
+              חשוב – כל עוד נציג לא הקיש 1 ,זמן ההמתנה ממשיך להיספר
+              ו
+            </p>
+          </div>
+        </div>
+      </div>
     </div>
   </div>
 </template>
@@ -76,9 +74,9 @@ export default {
     ]
   }),
   computed: {
-	  active(){
-		  return this.items.find(i => i.active);
-	  }
+    active() {
+      return this.items.find(i => i.active);
+    }
   },
   methods: {
     activateBox(id) {
