315c86a666d8fd3434073fcf5ff9074aed911410
Raymond Ativie <RaymondAtivie@gmail.com>
2019-09-13 10:26:55 +0100

    base CSS

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,40 +1,63 @@
 <template>
-  <div class="home">
-      <h1>כותרת ראשית H1 - גודל טקסט 30 פיקסל</h1>
-      <h2>כותרת משנה H2 - גודל טקטסט 25 פיקסל</h2>
-      <h3>כותרת רמה H3 - גודל טקסט 20 פיקסל</h3>
-      <p class="label">
-          תווית לשדה מילוי - בולד 15 פיקסל
-      </p>
-      <p class="field">
-          תוכן שדה למילוי - לייט 12 פיקסל
-      </p>
+    <div class="container home">
+        <h1>כותרת ראשית H1 - גודל טקסט 30 פיקסל</h1>
+        <h2>כותרת משנה H2 - גודל טקטסט 25 פיקסל</h2>
+        <h3>כותרת רמה H3 - גודל טקסט 20 פיקסל</h3>
 
-      <div class="row">
-          <div class="card ma-2">
-              <div class="bg-primary box"></div>
-          </div>
+        <p class="label">
+            תווית לשדה מילוי - בולד 15 פיקסל
+        </p>
+        <p class="field">
+            תוכן שדה למילוי - לייט 12 פיקסל
+        </p>
 
-          <div class="card ma-2">
-              <div class="bg-primary-secondary box"></div>
-          </div>
+        <div class="row">
+            <div class="col">
+                <div class="card m-2">
+                    <div class="bg-primary box"></div>
+                </div>
 
-          <div class="card ma-2">
-              <div class="bg-primary-light box"></div>
-          </div>
-      </div>
+                <div class="card m-2">
+                    <div class="bg-primary-secondary box"></div>
+                </div>
+            </div>
+        </div>
 
-      <div>
-          <div class="input-box" style="width: 400px">
-              <div class="label">תווית לשדה מילוי - בולד 15 פיקסל</div>
-              <input class="field" />
-          </div>
-      </div>
+        <div class="my-5">
+            <div class="input-box" style="width: 100%">
+                <div class="label">תווית לשדה מילוי - בולד 15 פיקסל</div>
+                <input class="field" />
+            </div>
+        </div>
 
-      <div>
-
-      </div>
-  </div>
+        <div class="row">
+            <div class="col">
+                <div class="card ma-2">
+                    <div class="bg-primary-light box"></div>
+                </div>
+            </div>
+            <div class="col">
+                <div class="card ma-2">
+                    <div class="bg-primary-light box"></div>
+                </div>
+            </div>
+            <div class="col">
+                <div class="card ma-2">
+                    <div class="bg-primary-light box"></div>
+                </div>
+            </div>
+        </div>
+        
+        <div class="row mt-5">
+            <div class="col">
+                <h3 class="t-secondary">כפתורים</h3>
+            </div>
+            <div class="col text-left">
+                <h3 class="t-secondary">Buttons</h3>
+            </div>
+        </div>
+        <hr />
+    </div>
 </template>
 
 <script>
