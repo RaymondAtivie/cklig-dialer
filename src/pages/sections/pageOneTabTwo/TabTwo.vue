1a7ff36395808b7cb02a78e64956c5a99f4071ad
GIDEON PETERS <gideonpeters85@gmail.com>
2019-09-14 11:17:04 +0100

    made some changes

---
+++ src/pages/sections/pageOneTabTwo/TabTwo.vue
@@ -0,0 +1,160 @@
+<template>
+	<div>
+		<ck-orange-box></ck-orange-box>
+
+		<div class="border-primary-bg-right mt-4">
+			<div class="d-flex mb">
+				<ck-box primary ml style="min-width: 220px">העברת שיחה אל</ck-box>
+				<ck-box select stretch>מספר יחיד להעברת שיחה</ck-box>
+			</div>
+			<div class="d-flex flex-row mb">
+				<div class="d-flex flex-column">
+					<ck-box primary ml style="min-width: 220px; height: 100%">
+						<div class="mb-2 align-self-start">תוכן ההודעה</div>
+					</ck-box>
+				</div>
+				<div class="d-flex flex-column border-accent-bg-right">
+					<div class="d-flex flex-row mb">
+						<ck-box block accent>מספר להעברת השיחה</ck-box>
+						<ck-box block light mr>052-6636199</ck-box>
+						<ck-box select light>+972</ck-box>
+					</div>
+					<div class="d-flex flex-row mb">
+						<ck-box disabled ml>העברת שיחה אל</ck-box>
+						<ck-box disabled stretch>מספר יחיד להעברת שיחה</ck-box>
+					</div>
+					<div class="d-flex flex-row">
+						<ck-box block accent>מספר להעברת השיחה</ck-box>
+						<ck-box block select light mr>0 שניות</ck-box>
+					</div>
+				</div>
+			</div>
+		</div>
+		<hr class="hr-blue my-3" />
+
+		<div class="border-primary-bg-right mt-4">
+			<div class="d-flex mb">
+				<ck-box primary ml style="min-width: 220px">העברת שיחה אל</ck-box>
+				<ck-box select stretch>מספר יחיד להעברת שיחה</ck-box>
+			</div>
+			<div class="d-flex mb">
+				<ck-box primary ml style="min-width: 220px">העברת שיחה אל</ck-box>
+				<ck-box select stretch>מספר יחיד להעברת שיחה</ck-box>
+			</div>
+			<div class="d-flex flex-row mb">
+				<div class="d-flex flex-column">
+					<ck-box primary ml style="min-width: 220px; height: 100%">
+						<div class="mb-2 align-self-start">תוכן ההודעה</div>
+					</ck-box>
+				</div>
+				<div class="d-flex flex-column border-accent-bg-right">
+					<div class="d-flex flex-row mb">
+						<ck-box block accent>מספר להעברת השיחה</ck-box>
+						<ck-box block light mr>052-6636199</ck-box>
+						<ck-box select light>+972</ck-box>
+					</div>
+					<div class="d-flex flex-row mb">
+						<ck-box disabled ml>העברת שיחה אל</ck-box>
+						<ck-box disabled stretch>מספר יחיד להעברת שיחה</ck-box>
+					</div>
+					<div class="d-flex flex-row">
+						<ck-box block accent>מספר להעברת השיחה</ck-box>
+						<ck-box block select light mr>0 שניות</ck-box>
+					</div>
+				</div>
+			</div>
+			<hr class="hr-blue my-3" />
+
+			<div class="d-flex flex-row mb">
+				<div class="d-flex flex-column">
+					<ck-box primary ml style="min-width: 220px; height: 100%">
+						<div class="mb-2 align-self-start">תוכן ההודעה</div>
+					</ck-box>
+				</div>
+				<div class="d-flex flex-column border-accent-bg-right">
+					<div class="d-flex flex-row mb">
+						<ck-box block accent>מספר להעברת השיחה</ck-box>
+						<ck-box block light mr>052-6636199</ck-box>
+						<ck-box select light>+972</ck-box>
+					</div>
+					<div class="d-flex flex-row mb">
+						<ck-box disabled ml>העברת שיחה אל</ck-box>
+						<ck-box disabled stretch>מספר יחיד להעברת שיחה</ck-box>
+					</div>
+					<div class="d-flex flex-row">
+						<ck-box block accent>מספר להעברת השיחה</ck-box>
+						<ck-box block select light mr>0 שניות</ck-box>
+					</div>
+				</div>
+			</div>
+
+			<hr class="hr-blue my-3" />
+
+			<div class="d-flex flex-row mt-4">
+				<div class="d-flex flex-column">
+					<ck-box primary ml style="min-width: 220px; height: 100%">
+						<div class="mb-2 align-self-start">תוכן ההודעה</div>
+					</ck-box>
+				</div>
+				<div class="d-flex flex-column border-accent-bg-right">
+					<div class="d-flex flex-row mb">
+						<ck-box block accent>מספר להעברת השיחה</ck-box>
+						<ck-box block light mr>052-6636199</ck-box>
+						<ck-box select light>+972</ck-box>
+					</div>
+					<div class="d-flex flex-row mb">
+						<ck-box disabled ml>העברת שיחה אל</ck-box>
+						<ck-box disabled stretch class v-text="'103 < 4 < 2 < 2'"></ck-box>
+					</div>
+					<div class="d-flex flex-row">
+						<ck-box block accent>מספר להעברת השיחה</ck-box>
+						<ck-box block select light mr>0 שניות</ck-box>
+					</div>
+				</div>
+			</div>
+		</div>
+
+		<hr class="hr-blue my-3" />
+
+		<div class="d-flex mb border-accent-bg-right">
+			<ck-box block accentDark style="min-width: 220px">מספר להעברת השיחה</ck-box>
+			<ck-box block accent mr br>052-6636199</ck-box>
+			<ck-box select block light>+972</ck-box>
+		</div>
+
+		<ck-orange-banner>הוסף מערך זמנים</ck-orange-banner>
+
+		<div class="border-primary-bg-right mt-4">
+			<div class="d-flex mb">
+				<ck-box stretch primary ml>מערך זמנים לביצוע הפעולה</ck-box>
+				<ck-box stretch>מספר יחיד להעברת שיחה</ck-box>
+			</div>
+			<div class="d-flex mb">
+				<ck-box primary ml style="min-width: 220px">העברת שיחה אל</ck-box>
+				<ck-box select stretch>מספר יחיד להעברת שיחה</ck-box>
+			</div>
+			<div class="d-flex flex-row mb">
+				<div class="d-flex flex-column">
+					<ck-box primary ml style="min-width: 220px; height: 100%">
+						<div class="mb-2 align-self-start">תוכן ההודעה</div>
+					</ck-box>
+				</div>
+				<div class="d-flex flex-column border-accent-bg-right">
+					<div class="d-flex flex-row mb">
+						<ck-box block accent>מספר להעברת השיחה</ck-box>
+						<ck-box block light mr>052-6636199</ck-box>
+						<ck-box select light>+972</ck-box>
+					</div>
+					<div class="d-flex flex-row mb">
+						<ck-box disabled ml>העברת שיחה אל</ck-box>
+						<ck-box disabled stretch>מספר יחיד להעברת שיחה</ck-box>
+					</div>
+					<div class="d-flex flex-row">
+						<ck-box block accent>מספר להעברת השיחה</ck-box>
+						<ck-box block select light mr>0 שניות</ck-box>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</template>
