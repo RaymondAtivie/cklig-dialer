12814c47716c6eee5b30a2a82ee1873ac2bcf313
GIDEON PETERS <gideonpeters85@gmail.com>
2019-09-16 18:18:58 +0100

    init page five

--- src/views/PageFive.vue
+++ src/views/PageFive.vue
@@ -1,5 +1,73 @@
 <template>
-	<div>
-		<h1>Page Five</h1>
+	<div class="container-fluid">
+		<div class="row justify-content-center">
+			<div class="col-11">
+				<h1 class="my-4">הגדרות למספר וירטואלי -073-2681986</h1>
+
+				<div class="row mb-4">
+					<div class="col-4">
+						<ck-input label="שם ייחודי למספר" />
+					</div>
+
+					<div class="col text-left">
+						<ck-button tile class="px-4">שיחות יוצאות</ck-button>
+						<ck-button light tile flat class="px-4">שיחות יוצאות</ck-button>
+						<ck-button light tile flat class="px-4">שיחות יוצאות</ck-button>
+						<ck-button light tile flat class="px-4">שיחות יוצאות</ck-button>
+					</div>
+				</div>
+
+				<ck-tabs v-model="selectedTab" :tabs="tabs">
+					<div class="border-bottom mb-4">
+						<div class="d-flex align-items-center mb-3">
+							<div class="stretch">
+								<h3 v-if="selectedTab == 1">הגדרות שיחה נכנסת</h3>
+								<h3
+									v-if="selectedTab == 2"
+								>פעולה טלפונית - השמעת הודעה שליחת סמס וסיום שיחה - בכל שעות היממה</h3>
+							</div>
+							<div class="ml-3">
+								<p class="t-disabled fs-08">עודכן בתאריך: 1986.8.26 בשעה 45:0</p>
+							</div>
+							<div class="text-left">
+								<ck-button small tile outline success class="px-3">שמור הגדרות</ck-button>
+							</div>
+						</div>
+					</div>
+
+					<div v-show="selectedTab == 1">
+						<PageFiveTabOne />
+					</div>
+					<div v-show="selectedTab == 2">
+						<PageFiveTabTwo />
+					</div>
+				</ck-tabs>
+			</div>
+		</div>
 	</div>
 </template>
+
+<script>
+import PageFiveTabOne from "./../pages/sections/PageFiveTabOne";
+import PageFiveTabTwo from "./../pages/sections/PageFiveTabTwo";
+
+export default {
+	components: {
+		PageFiveTabOne,
+		PageFiveTabTwo
+	},
+	data() {
+		return {
+			selectedTab: 1,
+			tabs: [
+				{ text: "הודעת פתיחה", value: 1 },
+				{ text: "זיהוי המתקשר", value: 2 },
+				{ text: "הגדרות שיחה נכנסת", value: 3 },
+				{ text: "בכל שעות היממה", value: 4 },
+				{ text: "הגדרות שיחה נכנסת", value: 5 },
+				{ text: "בכל שעות היממה", value: 6 }
+			]
+		};
+	}
+};
+</script>
