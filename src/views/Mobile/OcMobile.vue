15f756fff6755f391c041477749a8175418d1e19
David Joseph <davidjoe781227@gmail.com>
2019-11-19 17:36:36 +0100

    committing

---
+++ src/views/Mobile/OcMobile.vue
@@ -0,0 +1,22 @@
+<template>
+<div>
+    <div>
+        <oc-first></oc-first>
+    </div>
+    <div class="mt-5">
+        <oc-second></oc-second>
+    </div>
+</div>
+</template>
+<script>
+import ocMobileFirst from './ocMobileFirst'
+import ocMobileSecond from './ocMobileSecond'
+export default {
+    components: {
+        ocFirst: ocMobileFirst,
+        ocSecond: ocMobileSecond
+    }
+}
+</script>
+<style lang="scss" scoped>
+</style>
